4413114c08d5c0f28b5af4905f762bc388087b01
YomnaElsaeed <youmnaelsaeed43@gmail.com>
2025-01-01 14:27:23 +0200

    start

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-600">Hello</h1>
+    </div> 
+    </>
+  )
+}
+
+export default App
