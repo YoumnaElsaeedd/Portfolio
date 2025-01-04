89c8ad0467e10c4568612951921f4c11404aa326
YomnaElsaeed <youmnaelsaeed43@gmail.com>
2025-01-05 00:28:34 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './Components/Navbar'
 
 function App() {
 
 
   return (
     <>
-       <div className="text-center">
-      <h1 className="text-4xl font-bold text-blue-600">Hello</h1>
-    </div> 
+       <Navbar/>
     </>
   )
 }
