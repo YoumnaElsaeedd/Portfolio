b0c5d9a9cd5c08ac1d5a5e67aa468da7c1e7584f
YomnaElsaeed <youmnaelsaeed43@gmail.com>
2025-01-07 22:40:32 +0200

    edits

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -25,25 +25,13 @@ const Hero = () => {
           ]}
           speed={50}
           repeat={Infinity}
-          className="font-bold text-gray-400 text-xl md:text-5xl italic mb-6" // زيادة المسافة السفلية
-        />
-        <motion.p
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
-          transition={{ duration: 1, delay: 0.5 }}
-          className="text-gray-200 md:text-7xl text-5xl tracking-tight mb-6" // زيادة المسافة السفلية
-        >
-          Hey, I am <br />
-          <span className='text-purple-500'>Youmna Elsaeed</span>
+          className="font-bold text-gray-400 text-xl md:text-5xl italic mb-6"         />
+        <motion.p initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} viewport={{ once: true }} transition={{ duration: 1, delay: 0.5 }}
+          className="text-gray-200 md:text-7xl text-5xl tracking-tight mb-6"  >Hey, I am <br />
+        <span className='text-purple-500'>Youmna Elsaeed</span>
         </motion.p>
-        <motion.p
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          viewport={{ once: true }}
-          transition={{ duration: 1, delay: 1 }}
-          className="text-gray-300 md:text-2xl text-lg tracking-tight mb-4"
-        >
+        <motion.p initial={{ opacity: 0 }}whileInView={{ opacity: 1 }}viewport={{ once: true }}transition={{ duration: 1, delay: 1 }}
+          className="text-gray-300 md:text-2xl text-lg tracking-tight mb-4" >
           I am a passionate FrontEnd Developer
         </motion.p>
       </motion.div>
